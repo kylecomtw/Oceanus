56ba3084128e62f716e503f126520e7647032d56
SeanTseng <seantyh@gmail.com>
2017-09-18 21:05:11 +0800

    update bat file

--- run_oceanus.bat
+++ run_oceanus.bat
@@ -1,1 +1,1 @@
-java -cp Oceanus/target/Oceanus-0.2-jar-with-dependencies.jar;Oceanus/target/stanford-corenlp-model-3.8.0.jar;Oceanus/target/stanford-corenlp-chinese-model-3.8.0.jar tw.com.kyle.oceanus.nlp.SparkMain
+java -cp Oceanus/target/Oceanus-0.2.1-jar-with-dependencies.jar;Oceanus/target/stanford-corenlp-model-3.8.0.jar;Oceanus/target/stanford-corenlp-chinese-model-3.8.0.jar tw.com.kyle.oceanus.nlp.SparkMain
